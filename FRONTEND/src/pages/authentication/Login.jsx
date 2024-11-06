f9dcf59d0a67d7fe3db1d8aabb3580a2b980fbc8
deivid <julher2000@hotmail.es>
2024-11-06 10:32:26 -0500

    se inicio con el apartado del usuario externo

--- FRONTEND/src/pages/authentication/Login.jsx
+++ FRONTEND/src/pages/authentication/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [isLoading, setIsLoading] = useState(false);
-    const { setToken, setUserRole , setSession } = useContext(AuthContext);
+    const { setToken, setUserRole, setSession } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleSubmit = async (e) => {
@@ -42,10 +42,13 @@ export default function Login() {
     };
 
     return (
-        <div className="flex items-center justify-center bg-rose-100 w-full h-full">
-            <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
-                <h1 className="text-3xl font-bold text-center text-rose-600 mb-6">Mueve Villavo</h1>
-                <h2 className="text-2xl font-semibold text-center text-pink-700 mb-6">Iniciar Sesión</h2>
+
+        <div className="flex items-center justify-center bg-white w-full h-full">
+            <div className="bg-red-100 p-8 rounded-lg shadow-lg max-w-md w-full">
+                <div className="flex justify-center mb-6">
+                    <img src="./logo.png" className="w-24 h-24" />
+                </div>
+                <h2 className="text-2xl font-semibold text-center text-red-700 mb-6">Iniciar Sesión</h2>
                 <form className="space-y-4" onSubmit={handleSubmit}>
                     <Input
                         color="secondary"
@@ -56,7 +59,7 @@ export default function Login() {
                         required
                         fullWidth
                         size="lg"
-                        className="bg-pink-100 text-pink-200"
+                        className="bg-red-100 text-red-200"
                     />
                     <Input
                         color="secondary"
@@ -67,26 +70,19 @@ export default function Login() {
                         required
                         fullWidth
                         size="lg"
-                        className="bg-pink-50 text-pink-200"
+                        className="bg-red-50 text-red-200"
                     />
                     <Button
                         type="submit"
                         color="secondary"
                         size="lg"
-                        className="w-full bg-rose-400 hover:bg-rose-600 text-white"
+                        className="w-full bg-red-400 hover:bg-red-600 text-white"
                     >
                         {isLoading ? <Spinner color="white" /> : "Ingresar"}
                     </Button>
                 </form>
-                <div className="mt-6 text-center">
-                    <Link
-                        to="/register"
-                        className="text-rose-600 hover:text-rose-800 transition-colors duration-300"
-                    >
-                        ¿No tienes cuenta? Regístrate
-                    </Link>
-                </div>
             </div>
         </div>
     );
+        
 }
--- FRONTEND/src/pages/home/Home.jsx
+++ FRONTEND/src/pages/home/Home.jsx
@@ -6,25 +6,12 @@ import InteractiveMap from '../../components/InteractiveMap';
 
 const Home = () => {
   return (
-    <div className="relative flex h-screen w-full justify-center overflow-hidden">
-      {/* Mapa Interactivo de fondo */}
-      <div className="absolute inset-0 z-0  filter blur-[5px]">
-        <InteractiveMap />
-      </div>
-      
-      {/* Contenido superpuesto */}
-      <div className="relative flex  w-4/5 z-10">
-        {/* Menú lateral */}
-        
-
-        {/* Contenido principal */}
-        <div className="flex-grow w-4/5 mx-auto  bg-opacity-80 p-6">
-          <h1 className="text-pink-600 text-2xl mb-4 font-bold">Bienvenido a Villavicencio</h1>
-          <Notificaciones className="text-white" />
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <div className="flex 100vh 100wh h-full w-full">
+			<SideNav />
+			<div className="flex justify-center w-full h-full items-center">
+				
+			</div>
+		</div>
   );
 };
 
