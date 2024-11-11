1961248feae72300297aff589107b5f41a0d913a
deivid <julher2000@hotmail.es>
2024-11-10 21:50:37 -0500

    se termino de ajustar la seccion del externo

--- FRONTEND/src/navigation/AuthNavigation.jsx
+++ FRONTEND/src/navigation/AuthNavigation.jsx
@@ -2,13 +2,13 @@ import Login from "../pages/authentication/Login";
 import Register from "../pages/authentication/RegisterPage";
 import DeniedPage from "../pages/other/Denied";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Externo from "../pages/external/Externo"
+import Externo from "../pages/externo/Externo"
 
 export default function AuthNavigation() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Externo />} />
+                <Route path="/" element={<Externo/>} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />}/>
                 <Route path="*" element={<DeniedPage />} />
