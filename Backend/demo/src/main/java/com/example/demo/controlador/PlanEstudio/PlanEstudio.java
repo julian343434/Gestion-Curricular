79dfdbe085258fe21910ce05a04273342597493c
Dhanielt Felipe Rubio Prasca <132233063+dhanielt@users.noreply.github.com>
2024-11-25 20:59:20 -0500

    todo funcional solo faltan dos rutas

--- Backend/demo/src/main/java/com/example/demo/controlador/PlanEstudio/PlanEstudio.java
+++ Backend/demo/src/main/java/com/example/demo/controlador/PlanEstudio/PlanEstudio.java
@@ -11,8 +11,10 @@ import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -114,7 +116,7 @@ public class PlanEstudio {
                 relacion.setPlanEstudio(planEstudioEntidad);
                 
                 planEstudioEntidad.getCursos().add(relacion);
-                curso.getplanEstudio().add(relacion);
+                curso.getPlanEstudio().add(relacion);
             }
 
             // Guardar relaciones
@@ -124,7 +126,7 @@ public class PlanEstudio {
             return cursos;
 
         } catch (Exception e) {
-            throw new RuntimeException("Error al procesar el archivo: " + e.getMessage(), e);
+            throw new RuntimeException("Error al procesar la solicitud: " + e.getMessage(), e);
         }
     }
     
@@ -147,7 +149,7 @@ public class PlanEstudio {
 
         perteneceServicio.guardarRelacion(perteneceEntidad);
 
-        cursoEntidad.getplanEstudio().add(perteneceEntidad);
+        cursoEntidad.getPlanEstudio().add(perteneceEntidad);
         
         planEstudioEntidad.getCursos().add(perteneceEntidad);
 
@@ -173,7 +175,7 @@ public class PlanEstudio {
             perteneceEntidad.setPlanEstudio(planEstudioEntidad);
             perteneceEntidad.setCurso(curso);
 
-            curso.getplanEstudio().add(perteneceEntidad);
+            curso.getPlanEstudio().add(perteneceEntidad);
             perteneceServicio.guardarRelacion(perteneceEntidad);
         }
 
@@ -182,6 +184,94 @@ public class PlanEstudio {
         }
         
         return curso;
+    }
+
+    @PatchMapping("/planEstudio/{id}")
+    @PreAuthorize("hasRole('Administrador')") 
+    public ResponseEntity<Object> actualizarPlanEstudio(@PathVariable Long id,
+                                        @RequestParam(value = "file", required = false) MultipartFile archivo,
+                                        @RequestParam(value = "nombre", required = false) String nombre,
+                                        @RequestParam(value = "descripcion", required = false) String descripcion,
+                                        @RequestParam(value = "anio", required = false) Integer anio) {
+        try {
+            Object resultado = null;
+            boolean devolver = false;
+            // Procesar archivo si está presente
+            if (archivo != null) {
+                if (anio == null) {
+                    throw new RuntimeException("Falta 'anio' para actualizar el archivo de plan de estudio");
+                }
+
+                if (archivo.isEmpty()) {
+                    throw new RuntimeException("El archivo está vacío");
+                }
+
+                try (InputStream inputStream = archivo.getInputStream();
+                    Workbook workbook = new XSSFWorkbook(inputStream)) {
+                    Sheet hoja = workbook.getSheetAt(0);
+                    if (hoja == null) {
+                        throw new RuntimeException("La hoja está vacía");
+                    }
+
+                    validarSiEsExcel(archivo);
+
+                    cursoServicio.eliminarCursoArchivo(id);
+
+                    PlanEstudioEntidad planEstudioEntidad = planEstudioServicio.buscarId(id);
+                    planEstudioEntidad.setArchivo(archivo.getBytes());
+
+                    List<CursoEntidad> cursos = cursoServicio.crearCursoArchivo(hoja);
+                    List<Long> idCursos = new ArrayList<>();
+                    for (CursoEntidad curso : cursos) {
+                        idCursos.add(curso.getId_curso());
+                    }
+
+                    List<PerteneceEntidad> relaciones = perteneceServicio.crearRelacionArchivo(
+                            planEstudioEntidad.getId_plan_estudio(), idCursos, anio);
+
+                    for (int i = 0; i < cursos.size(); i++) {
+                        PerteneceEntidad relacion = relaciones.get(i);
+                        CursoEntidad curso = cursos.get(i);
+
+                        relacion.setCurso(curso);
+                        relacion.setPlanEstudio(planEstudioEntidad);
+
+                        planEstudioEntidad.getCursos().add(relacion);
+                        curso.getPlanEstudio().add(relacion);
+                    }
+
+                    perteneceServicio.guardarRelacionArchivo(relaciones);
+                    
+                    devolver = true;
+                    resultado = cursos;
+                }
+            }
+
+            // Actualizar otros campos si están presentes
+            PlanEstudioEntidad planEstudioEntidad = planEstudioServicio.buscarId(id);
+            if (nombre != null) {
+                planEstudioEntidad.setNombre(nombre);
+            }
+            if (descripcion != null) {
+                planEstudioEntidad.setDescripcion(descripcion);
+            }
+            if(!devolver){
+                resultado = planEstudioEntidad;
+            }
+            planEstudioServicio.guardarPlan(planEstudioEntidad);
+
+            return ResponseEntity.ok(resultado);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error al procesar la solicitud: " + e.getMessage(), e);
+        }
+    }
+
+    @DeleteMapping("/cursos/{id}")
+    @PreAuthorize("hasRole('Administrador')") 
+    public ResponseEntity<String> borrarCurso(@PathVariable Long id){
+        cursoServicio.eliminarCurso(id);
+        return ResponseEntity.ok("curso eliminado correctamente");
     }
 
     // Método de validación
@@ -193,6 +283,4 @@ public class PlanEstudio {
             throw new RuntimeException("El archivo no es un Excel válido. Asegúrate de subir un archivo .xls o .xlsx");
         }
     }
-    
-
 }
--- Backend/demo/src/main/java/com/example/demo/modelo/PlanEstudio/repositorios/CursoRepositorio.java
+++ Backend/demo/src/main/java/com/example/demo/modelo/PlanEstudio/repositorios/CursoRepositorio.java
@@ -1,11 +1,20 @@
 package com.example.demo.modelo.PlanEstudio.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.modelo.PlanEstudio.entidades.CursoEntidad;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface CursoRepositorio extends JpaRepository<CursoEntidad, Long>{
-
+    
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM curso WHERE id_curso IN (SELECT id_curso FROM pertenece WHERE id_plan_estudio = :idPlanEstudio)", nativeQuery = true)
+    void deleteCursosByPlanEstudio(@Param("idPlanEstudio") Long idPlanEstudio);
 }
